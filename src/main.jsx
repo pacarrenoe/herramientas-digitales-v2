0185357652a85c8baac37536487059753a0995c7
pacarrenoe <72708319+pacarrenoe@users.noreply.github.com>
2026-09-08 15:53:59 -0300

    fix: compact reader and enable fullscreen viewer

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
-import "/src/styles/global.css";
+import App from "./app/App";
+import "./app/styles/global.css";
 
 
 ReactDOM.createRoot(document.getElementById("root")).render(
